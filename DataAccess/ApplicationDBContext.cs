2988aec3032ffd561215143a760b88be495a2770
Valentin <valentinale203@gmail.com>
2023-09-24 17:05:49 -0300

    Agragar clase ApplicationDbContext

---
+++ DataAccess/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Integrador.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Integrador.DataAccess
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Trabajo> Trabajos { get; set; }
+        public DbSet<Servicio> Servicios { get; set; }
+        public DbSet<Proyecto> Proyecto { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+
+    }
+}
