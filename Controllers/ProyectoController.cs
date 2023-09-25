f22f92187743f5bcd23831d84c663a649a3ad253
Valentin <valentinale203@gmail.com>
2023-09-25 20:45:57 -0300

    Agregar Controlladoras ProyectoController, ServicioController, TrabajoController y modificacion en el UnitOfWork

--- Controllers/ProyectoController.cs
+++ Controllers/ProyectoController.cs
@@ -8,12 +8,12 @@ namespace TechOil.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TrabajoController : ControllerBase
+    public class ProyectoController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
 
-        public TrabajoController(IMapper mapper, IUnitOfWork unitOfWork)
+        public ProyectoController(IMapper mapper, IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
@@ -23,11 +23,11 @@ namespace TechOil.Controllers
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IEnumerable<TrabajoDTO>>> GetAllTrabajos()
+        public async Task<ActionResult<IEnumerable<ProyectoDTO>>> GetAllProyectos()
         {
-            IEnumerable<Trabajo> trabajosList = await _unitOfWork.TrabajoRepository.GetAll();
+            IEnumerable<Proyecto> proyectosList = await _unitOfWork.ProyectoRepository.GetAll();
 
-            return Ok(_mapper.Map<IEnumerable<TrabajoDTO>>(trabajosList));
+            return Ok(_mapper.Map<IEnumerable<ProyectoDTO>>(proyectosList));
         }
 
 
@@ -35,37 +35,37 @@ namespace TechOil.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<TrabajoDTO>> PostTrabajo([FromBody] TrabajoDTO trabajoDto)
+        public async Task<ActionResult<ProyectoDTO>> PostProyecto([FromBody] ProyectoDTO proyectoDto)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            if (trabajoDto == null)
+            if (proyectoDto == null)
             {
-                return BadRequest(trabajoDto);
+                return BadRequest(proyectoDto);
             }
 
-            Trabajo trabajoModel = _mapper.Map<Trabajo>(trabajoDto);
-            await _unitOfWork.TrabajoRepository.Insert(trabajoModel);
+            Proyecto proyectoModel = _mapper.Map<Proyecto>(proyectoDto);
+            await _unitOfWork.ProyectoRepository.Insert(proyectoModel);
 
-            return CreatedAtRoute("GetTrabajoById", new { id = trabajoDto.IdTrabajo }, trabajoDto);
+            return CreatedAtRoute("GetProyectoById", new { id = proyectoDto.IdProyecto }, proyectoDto);
         }
 
 
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> PutTrabajo(int id, [FromBody] TrabajoDTO trabajoDto)
+        public async Task<IActionResult> PutProyecto(int id, [FromBody] ProyectoDTO proyectoDto)
         {
-            if (trabajoDto == null || id != trabajoDto.IdTrabajo)
+            if (proyectoDto == null || id != proyectoDto.IdProyecto)
             {
                 return BadRequest();
             }
 
-            Trabajo trabajoModel = _mapper.Map<Trabajo>(trabajoDto);
-            await _unitOfWork.TrabajoRepository.Update(trabajoModel);
+            Proyecto proyectoModel = _mapper.Map<Proyecto>(proyectoDto);
+            await _unitOfWork.ProyectoRepository.Update(proyectoModel);
 
             return NoContent();
         }
@@ -75,18 +75,17 @@ namespace TechOil.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> DeleteTrabajo(int IdTrabajo)
+        public async Task<IActionResult> DeleteProyecto(int IdProyecto)
         {
-            if (IdTrabajo == 0)
+            if (IdProyecto == 0)
             {
                 return BadRequest();
             }
             else
             {
-                await _unitOfWork.TrabajoRepository.Delete(IdTrabajo);
+                await _unitOfWork.ProyectoRepository.Delete(IdProyecto);
                 return NoContent();
             }
         }
     }
 }
-
--- DataAccess/Repositories/ProyectoRepository.cs
+++ DataAccess/Repositories/ProyectoRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace TechOil.DataAccess.Repositories
+﻿using Integrador.DataAccess;
+using Integrador.Models;
+using Microsoft.EntityFrameworkCore;
+using TechOil.DataAccess.Repositories.Interfaces;
+
+namespace TechOil.DataAccess.Repositories
 {
-    public class ProyectoRepository
+    public class ProyectoRepository : Repository<Proyecto>, IProyectoRepository
     {
+
+        public ProyectoRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public override async Task<bool> Update(Proyecto updateProyecto)
+        {
+            var Proyecto = await _context.Proyecto.FirstOrDefaultAsync(x => x.IdProyecto == updateProyecto.IdProyecto);
+            if (Proyecto == null) { return false; }
+
+            Proyecto.Nombre = updateProyecto.Nombre;
+            Proyecto.Direccion = updateProyecto.Direccion;
+            Proyecto.Estado = updateProyecto.Estado;
+            Proyecto.Activo = updateProyecto.Activo;
+            _context.Proyecto.Update(Proyecto);
+            return true;
+        }
+
+        public override async Task<bool> Delete(int id)
+        {
+            var Proyecto = await _context.Proyecto.Where(x => x.IdProyecto == id).FirstOrDefaultAsync();
+            if (Proyecto != null)
+            {
+                _context.Proyecto.Remove(Proyecto);
+            }
+
+            return true;
+        }
     }
 }
